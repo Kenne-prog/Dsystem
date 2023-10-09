81abe79d94882d8318a42932a27d2998f8d489be
Kenneth <nonveragain12345@gmail.com>
2023-10-09 14:57:42 -0700

    test

--- Project1/master.py
+++ Project1/master.py
@@ -1,31 +1,45 @@
 import socket
 
-# Create a socket for listening to incoming connections
+
+# Create a socket for multicasting
+multicast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+multicast_group = '224.1.1.1'
+multicast_port = 6002
+
+# Bind the multicast socket to the multicast group and port
+multicast_socket.bind(('0.0.0.0', multicast_port))
+
+# Join the multicast group
+multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP,
+                            socket.inet_aton(multicast_group) + socket.inet_aton('0.0.0.0'))
+
+# Create a socket for listening to incoming connections (TCP)
 master_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-master_address = ('0.0.0.0', 6000)  # Listen on all available network interfaces, port 6000
+master_address = ('0.0.0.0', 6000)
 
 master_socket.bind(master_address)
-
-# Listen for incoming connections (up to 4 nodes in this example)
 master_socket.listen(4)
 
 # List to store connected nodes' information
 connected_nodes = []
 
-# ...
-
 while True:
-    # Accept incoming connection from a node
+    # Accept incoming connection from a node (TCP)
     node_socket, node_address = master_socket.accept()
 
     # Store information about the connected node
     connected_nodes.append((node_socket, node_address))
 
-    # Send a message from the master to all connected nodes
+    # Send a message from the master to all connected nodes (TCP)
     message = "This is a message from the master to all nodes!"
     for node_sock, _ in connected_nodes:
         try:
             node_sock.sendall(message.encode())
         except BrokenPipeError:
             # Handle the BrokenPipeError (node closed the connection)
-            node_sock.close()  # Close the socket
+            node_sock.close()
+
+    # Send a multicast message
+    multicast_message = "This is a multicast message from the master!"
+    multicast_socket.sendto(multicast_message.encode(), (multicast_group, multicast_port))
--- Project1/node.py
+++ Project1/node.py
@@ -1,17 +1,36 @@
 import socket
 
-# Create a socket for communication with the master server
+# Create a socket for communication with the master server (TCP)
 node_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 master_address = ('master', 6000)  # Replace with the actual IP address of the master server
 
 try:
-    # Connect to the master server
+    # Connect to the master server (TCP)
     node_socket.connect(master_address)
 
-    # Receive a message from the master
+    # Receive a message from the master (TCP)
     data = node_socket.recv(1024).decode()
     print(f"Received message from the master: {data}")
 
 finally:
-    # Close the connection with the master
+    # Close the connection with the master (TCP)
     node_socket.close()
+
+
+# Create a socket for listening to multicast messages
+multicast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+multicast_group = '224.1.1.1'
+multicast_port = 6002
+
+# Bind the multicast socket to the multicast group and port
+multicast_socket.bind(('0.0.0.0', multicast_port))
+
+# Join the multicast group
+multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP,
+                            socket.inet_aton(multicast_group) + socket.inet_aton('0.0.0.0'))
+
+while True:
+
+    # Receive multicast messages
+    multicast_data, _ = multicast_socket.recvfrom(1024)
+    print(f"Received multicast message: {multicast_data.decode()}")
