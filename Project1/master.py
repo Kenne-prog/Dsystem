5aa5ec9c65d279efe4c8cae2bf19281a0802741c
Kenneth <nonveragain12345@gmail.com>
2023-10-09 23:26:22 -0700

    evrtyhui

--- Project1/master.py
+++ Project1/master.py
@@ -1,45 +1,48 @@
 import socket
+def main():
+    # Create a socket for multicasting
+    multicast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+    multicast_group = '224.1.1.1'
+    multicast_port = 6006
 
-# Create a socket for multicasting
-multicast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Bind the multicast socket to the multicast group and port
+    multicast_socket.bind(('0.0.0.0', multicast_port))
 
-multicast_group = '224.1.1.1'
-multicast_port = 6002
+    # Join the multicast group
+    multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP,
+                                socket.inet_aton(multicast_group) + socket.inet_aton('0.0.0.0'))
 
-# Bind the multicast socket to the multicast group and port
-multicast_socket.bind(('0.0.0.0', multicast_port))
+    # Create a socket for listening to incoming connections (TCP)
+    master_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    master_address = ('0.0.0.0', 6000)
 
-# Join the multicast group
-multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP,
-                            socket.inet_aton(multicast_group) + socket.inet_aton('0.0.0.0'))
+    master_socket.bind(master_address)
+    master_socket.listen(4)
 
-# Create a socket for listening to incoming connections (TCP)
-master_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-master_address = ('0.0.0.0', 6000)
+    # List to store connected nodes' information
+    connected_nodes = []
 
-master_socket.bind(master_address)
-master_socket.listen(4)
+    while True:
+        # Accept incoming connection from a node (TCP)
+        node_socket, node_address = master_socket.accept()
 
-# List to store connected nodes' information
-connected_nodes = []
+        # Store information about the connected node
+        connected_nodes.append((node_socket, node_address))
 
-while True:
-    # Accept incoming connection from a node (TCP)
-    node_socket, node_address = master_socket.accept()
+        # Send a message from the master to all connected nodes (TCP)
+        message = "This is a message from the master to all nodes!"
+        for node_sock, _ in connected_nodes:
+            try:
+                node_sock.sendall(message.encode())
+            except BrokenPipeError:
+                # Handle the BrokenPipeError (node closed the connection)
+                node_sock.close()
+                connected_nodes.remove((node_sock, _))
 
-    # Store information about the connected node
-    connected_nodes.append((node_socket, node_address))
+        # Send a multicast message
+        multicast_message = "This is a multicast message from the master!"
+        multicast_socket.sendto(multicast_message.encode(), (multicast_group, multicast_port))
 
-    # Send a message from the master to all connected nodes (TCP)
-    message = "This is a message from the master to all nodes!"
-    for node_sock, _ in connected_nodes:
-        try:
-            node_sock.sendall(message.encode())
-        except BrokenPipeError:
-            # Handle the BrokenPipeError (node closed the connection)
-            node_sock.close()
-
-    # Send a multicast message
-    multicast_message = "This is a multicast message from the master!"
-    multicast_socket.sendto(multicast_message.encode(), (multicast_group, multicast_port))
+if __name__=='__main__':
+    main()
--- Project1/node.py
+++ Project1/node.py
@@ -1,36 +1,59 @@
 import socket
+import time
+import csv
 
-# Create a socket for communication with the master server (TCP)
-node_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-master_address = ('master', 6000)  # Replace with the actual IP address of the master server
+communication = 'Communication.csv'
+def main():
+    
+    # Create a set to store received multicast messages
+    received_messages = set()
 
-try:
-    # Connect to the master server (TCP)
-    node_socket.connect(master_address)
+    # Create a socket for communication with the master server (TCP)
+    node_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    master_address = ('master', 6000)  # Replace with the actual IP address of the master server
 
-    # Receive a message from the master (TCP)
-    data = node_socket.recv(1024).decode()
-    print(f"Received message from the master: {data}")
+    try:
+        # Connect to the master server (TCP)
+        node_socket.connect(master_address)
 
-finally:
-    # Close the connection with the master (TCP)
-    node_socket.close()
+        # Receive a message from the master (TCP)
+        data = node_socket.recv(1024).decode()
+        print(f"Received message from the master: {data}")
+        log_communication("Broadcast", socket.gethostbyname('master'), 6000, socket.gethostbyname(socket.gethostname()), 6000, len(data))
 
+    finally:
+        # Close the connection with the master (TCP)
+        node_socket.close()
 
-# Create a socket for listening to multicast messages
-multicast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-multicast_group = '224.1.1.1'
-multicast_port = 6002
+    # Create a socket for listening to multicast messages
+    multicast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    multicast_group = '224.1.1.1'
+    multicast_port = 6006
 
-# Bind the multicast socket to the multicast group and port
-multicast_socket.bind(('0.0.0.0', multicast_port))
+    # Bind the multicast socket to the multicast group and port
+    multicast_socket.bind(('0.0.0.0', multicast_port))
 
-# Join the multicast group
-multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP,
-                            socket.inet_aton(multicast_group) + socket.inet_aton('0.0.0.0'))
+    # Join the multicast group
+    multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP,
+                                socket.inet_aton(multicast_group) + socket.inet_aton('0.0.0.0'))
 
-while True:
+    while True:
+        # Receive multicast messages
+        multicast_data, _ = multicast_socket.recvfrom(1024)
+        message = multicast_data.decode()
 
-    # Receive multicast messages
-    multicast_data, _ = multicast_socket.recvfrom(1024)
-    print(f"Received multicast message: {multicast_data.decode()}")
+        # Check if the message has already been received to deduplicate
+        if message not in received_messages:
+            received_messages.add(message)
+            print(f"Received multicast message: {message}")
+            log_communication("Multicast", '0.0.0.0', multicast_port, multicast_group, multicast_port, len(message))
+
+def log_communication(protocol, source_ip, source_port, destination_ip, destination_port, length):
+    current_time = time.time()
+    log_entry = [protocol, current_time, source_ip, destination_ip, source_port, destination_port, length]
+
+    with open(communication, 'a', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(log_entry)
+if __name__=='__main__':
+    main()
